411b017628bea733b973121a610d7726b0c152dc
CPKVG <51846930+CPKVG@users.noreply.github.com>
2021-07-31 01:22:44 +1200

    gitignore is now working

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebase:{
-    apiKey: "AIzaSyAVMDslZ04Z_8D8BgVSfatuc_Q5tqZxVbc",
-    authDomain: "survey-cms-app-9cac1.firebaseapp.com",
-    projectId: "survey-cms-app-9cac1",
-    storageBucket: "survey-cms-app-9cac1.appspot.com",
-    messagingSenderId: "566860596976",
-    appId: "1:566860596976:web:07eebed701c1bf63d36435",
-    measurementId: "G-4QXJSJG0D5"
+
   }
 };
